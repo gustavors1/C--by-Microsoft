4abc1c7974cd3bba8e2397812e19018455361240
gusta <gustavoandres_r@hotmail.com>
2023-08-16 16:15:01 +0200

    Primer Programa para comprender la sintaxis.

---
+++ 01_HelloWord.cs
@@ -0,0 +1,8 @@
+// Console.WriteLine("Hello World");
+
+Console.Write("Congratulations");
+Console.Write(" ");
+Console.Write("You wrote yoy firts lines of code.");
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
