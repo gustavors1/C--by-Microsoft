374724382be00b72aa5c0b475caa097930a53128
gusta <gustavoandres_r@hotmail.com>
2023-08-17 19:59:23 +0200

    Operaciones Matemáticas

--- 04_numericalOperations.cs
+++ 04_numericalOperations.cs
@@ -123,3 +123,87 @@ Console.WriteLine(value2);
 Output:
     23
     35
+
+// Incremento y decremento de valores
+
+/* El operador += suma y asigna el valor situado a la derecha del operador 
+al valor situado a la izquierda del operador. 
+Por lo tanto, las líneas dos y tres del siguiente fragmento de código son las mismas:
+*/
+
+int value = 0;     // value is now 0.
+value = value + 5; // value is now 5.
+value += 5;        // value is now 10.
+
+/* El operador ++ incrementa el valor de la variable en 1. 
+Por lo tanto, las líneas dos y tres del siguiente fragmento de código son las mismas:
+*/
+
+int value = 0;     // value is now 0.
+value = value + 1; // value is now 1.
+value++;           // value is now 2.
+
+// Los operadores como +=, -=, *=, ++ y -- se conocen como operadores de asignación compuesta.
+
+int value = 1;
+
+value = value + 1;
+Console.WriteLine("First increment: " + value);
+
+value += 1;
+Console.WriteLine("Second increment: " + value);
+
+value++;
+Console.WriteLine("Third increment: " + value);
+
+value = value - 1;
+Console.WriteLine("First decrement: " + value);
+
+value -= 1;
+Console.WriteLine("Second decrement: " + value);
+
+value--;
+Console.WriteLine("Third decrement: " + value);
+
+Output:
+    First increment: 2
+    Second increment: 3
+    Third increment: 4
+    First decrement: 3
+    Second decrement: 2
+    Third decrement: 1
+
+// Colocación de los operadores de incremento y decremento
+
+/*  usa el operador delante del valor como en ++value, 
+el incremento se producirá antes de recuperar el valor. 
+Del mismo modo, value++ incrementará el valor una vez recuperado.*/
+
+int value = 1;
+value++;
+Console.WriteLine("First: " + value);
+Console.WriteLine("Second: " + value++);
+Console.WriteLine("Third: " + value);
+Console.WriteLine("Fourth: " + (++value));
+
+Output: 
+    First: 2
+    Second: 2
+    Third: 3
+    Fourth: 4
+
+// Cálculo de los grados Celsius según la temperatura actual en grados Fahrenheit
+
+int fahrenheit = 94;
+int resta = 32;
+decimal multiplicacion = 5.0m / 9.0m;
+decimal cOperantion = (decimal)fahrenheit - (decimal)resta;
+Console.WriteLine(cOperantion);
+decimal celsius = cOperantion * multiplicacion;
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
+
+// Solución sugerida por el curso
+
+int fahrenheit = 94;
+decimal celsius = (fahrenheit - 32m) * (5m / 9m);
+Console.WriteLine("The temperature is " + celsius + " Celsius.");
